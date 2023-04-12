6c351c93112e517133f8f4d1d27953994edbca9f
Charles JUDITH <contact@charlesjudith.com>
2023-04-12 20:55:48 +0200

    Update app.py, add html templates with navbar

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from pycoingecko import CoinGeckoAPI
 import redis
 from typing import Dict
@@ -19,23 +21,47 @@ REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_DB = os.environ.get('REDIS_DB', 0)
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', None)
 
-app = FastAPI()
+app = FastAPI(title="Crypto Prices API", description="An API for getting cryptocurrency prices", version="1.0.0")
 
 # initialize CoinGecko API client and Redis client
 cg = CoinGeckoAPI()
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=None)
 
-# set cache expiration time to 5 minutes
-cache_expiration_time = 300
+# set cache expiration time to 15 minutes
+cache_expiration_time = 900
+
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def home(request: Request):
+    api_info = {
+        "title": "My Crypto Prices API",
+        "description": "An API for getting cryptocurrency prices",
+        "version": "1.0.0"
+    }
+    return templates.TemplateResponse("index.html", {"request": request, "api_info": api_info})
 
 # define API endpoint to get all available symbols
 @app.get('/symbols', response_model=List[str])
 async def get_crypto_symbols():
     """Get all available symbols"""
+    cache_key = "symbols"
+    cached_symbols = redis_client.get(cache_key)
+    if cached_symbols is not None:
+        print("Fetching all available symbols from cache...")
+        symbols = cached_symbols.decode().split(',')
+        return symbols
+
     print("Fetching all available symbols from CoinGecko API...")
     coins_list = cg.get_coins_list()
-    return [coin["id"] for coin in coins_list]
+    symbols = [coin["id"] for coin in coins_list]
+
+    # cache the symbols in Redis
+    redis_client.setex(cache_key, cache_expiration_time, ','.join(symbols))
+
+    return symbols
 
 # define API endpoint to get the price of a cryptocurrency by symbol
 @app.get('/price', response_model=List[PriceResponse])
